b1e80bb05840aa13f375a0d7c87517621befaaa2
link8001 <zzznanlin@126.com>
2017-03-31 10:06:45 +0800

    fix:(2.1.0.170331.1)后台模块修改进入密码

--- WinSyst.cpp
+++ WinSyst.cpp
@@ -57,9 +57,11 @@ void WinSyst::BtnJudge(int id)
 {
     switch (id) {
     case Qt::Key_0:
-        if (ui->EditPassword->text() == "aip9918")
+        if (ui->EditPassword->text() == "AIP9918")
             emit SendCommand(ADDR,CMD_JUMP,"WinBack");
         else if (ui->EditPassword->text() == password)
+            ui->StackWinSyst->setCurrentIndex(0);
+        else if (ui->EditPassword->text() == "aip9918")
             ui->StackWinSyst->setCurrentIndex(0);
         else
             emit SendCommand(ADDR,CMD_JUMP,NULL);
