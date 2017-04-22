160ea42fa53fd2a9a8a9e77df1710182658a91fc
zhaonanlin <zzznanlin@126.com>
2017-04-22 10:03:19 +0800

    修复:(2.1.0.170422)波形显示修正溢出问题

--- Waveform.cpp
+++ Waveform.cpp
@@ -19,8 +19,6 @@ Waveform::Waveform(QWidget *parent) : QLabel(parent)
     isTest = false;
     isItem = false;
     isBlock = false;
-    WaveLine = 0;
-    WaveGroup = 0;
     Block0 = 5;
     Block1 = 395;
     for (int i=0; i<MAX_WAVE; i++) {
@@ -46,12 +44,11 @@ void Waveform::WaveByteShow(QByteArray msg)
         for (int i=0; i<WaveByte.size()/2; i++)
             WaveByteH.append(quint16(WaveByte.at(i*2))*256+quint8(WaveByte.at(i*2+1)));
     }
-
     for (int i=0; i<this->width(); i++) {
         int t = i*WaveByteH.size()/this->width();
         if (WaveByte.size() == 800)
             WaveByteY.append(this->height()-WaveByteH.at(t)*this->height()/1024);
-        else
+        if (WaveByte.size() == 400)
             WaveByteY.append(this->height()-WaveByteH.at(t)*this->height()/256);
     }
     this->update();
@@ -73,7 +70,7 @@ void Waveform::WaveTestShow(QByteArray msg)
         int t = i*WaveTestH.size()/this->width();
         if (WaveTestH.size() == 400)
             WaveTestY.append(this->height()-WaveTestH.at(t)*this->height()/1024);
-        else
+        if (WaveTestH.size() == 800)
             WaveTestY.append(this->height()-WaveTestH.at(t)*this->height()/65536);
     }
     isTest = true;
@@ -94,7 +91,7 @@ void Waveform::WaveItemShow(QByteArray msg)
   * @param  b1:startup line postion;b2:end line postion.
   * @retval None
   */
-void Waveform::WaveBlock(int b1, int b2)
+void Waveform::WaveBlock(int b1,  int b2)
 {
     Block0 = b1;
     Block1 = b2;
@@ -135,36 +132,36 @@ void Waveform::InitWaveTest(quint8 s)
 void Waveform::paintEvent(QPaintEvent *)
 {
     QPainter *painter = new QPainter(this);
-    painter->setPen(QPen(Qt::darkGreen, 1, Qt::DotLine));
+    painter->setPen(QPen(Qt::darkGreen,  1,  Qt::DotLine));
     //横线
-    painter->drawLine(QPoint(0,height()*3/4),QPoint(width(),height()*3/4));
-    painter->drawLine(QPoint(0,height()*2/4),QPoint(width(),height()*2/4));
-    painter->drawLine(QPoint(0,height()*1/4),QPoint(width(),height()*1/4));
+    painter->drawLine(QPoint(0, height()*3/4), QPoint(width(), height()*3/4));
+    painter->drawLine(QPoint(0, height()*2/4), QPoint(width(), height()*2/4));
+    painter->drawLine(QPoint(0, height()*1/4), QPoint(width(), height()*1/4));
     //坚线
-    painter->drawLine(QPoint(width()*1/4,0),QPoint(width()*1/4,height()));
-    painter->drawLine(QPoint(width()*2/4,0),QPoint(width()*2/4,height()));
-    painter->drawLine(QPoint(width()*3/4,0),QPoint(width()*3/4,height()));
+    painter->drawLine(QPoint(width()*1/4, 0), QPoint(width()*1/4, height()));
+    painter->drawLine(QPoint(width()*2/4, 0), QPoint(width()*2/4, height()));
+    painter->drawLine(QPoint(width()*3/4, 0), QPoint(width()*3/4, height()));
 
-    painter->setPen(QPen(Qt::green, 1, Qt::SolidLine));
+    painter->setPen(QPen(Qt::green,  1,  Qt::SolidLine));
     for (int i=0; i<WaveByteY.size()-1; i++) {
-        painter->drawLine(QPoint(i,WaveByteY.at(i)),QPoint(i+1,WaveByteY.at(i+1)));
+        painter->drawLine(QPoint(i, WaveByteY.at(i)), QPoint(i+1, WaveByteY.at(i+1)));
     }
     if (isTest) {
-        painter->setPen(QPen(Qt::white, 1, Qt::SolidLine));
+        painter->setPen(QPen(Qt::white,  1,  Qt::SolidLine));
         for (int i=0; i<WaveTestY.size()-1; i++) {
-            painter->drawLine(QPoint(i,WaveTestY.at(i)),QPoint(i+1,WaveTestY.at(i+1)));
+            painter->drawLine(QPoint(i, WaveTestY.at(i)), QPoint(i+1, WaveTestY.at(i+1)));
         }
     }
     if (isItem) {
-        painter->drawText(QPoint(width()*3/4,height()*1/4-5),WaveItem);
+        painter->drawText(QPoint(width()*3/4, height()*1/4-5), WaveItem);
     }
     if (isBlock) {
         int b0 = Block0*this->width()/400;
         int b1 = Block1*this->width()/400;
-        painter->setPen(QPen(Qt::yellow, 1, Qt::SolidLine));
-        painter->drawLine(QPoint(b0,0),QPoint(b0,height()));
-        painter->setPen(QPen(Qt::red, 1, Qt::SolidLine));
-        painter->drawLine(QPoint(b1,0),QPoint(b1,height()));
+        painter->setPen(QPen(Qt::yellow,  1,  Qt::SolidLine));
+        painter->drawLine(QPoint(b0, 0), QPoint(b0, height()));
+        painter->setPen(QPen(Qt::red,  1,  Qt::SolidLine));
+        painter->drawLine(QPoint(b1, 0), QPoint(b1, height()));
     }
     painter->end();
 }
@@ -179,7 +176,7 @@ void Waveform::resizeEvent(QResizeEvent *)
     if (isTest)
         WaveTestShow(WaveTest);
     if (isBlock)
-        WaveBlock(Block0,Block1);
+        WaveBlock(Block0, Block1);
 }
 /**
   * @brief  Click to change block
--- WinHome.cpp
+++ WinHome.cpp
@@ -18,7 +18,7 @@ WinHome::WinHome(QWidget *parent) :
     ui->setupUi(this);
     InitWindows();
     InitButtons();
-    InitVersion("V-2.1.0.170421");
+    InitVersion("V-2.1.0.170422");
     HomeMode = HOME_FREE;
     InitThreadAll();
 }
